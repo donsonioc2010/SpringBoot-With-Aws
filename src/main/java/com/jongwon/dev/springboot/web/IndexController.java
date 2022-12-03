18fcbad2527633e9eb1ecba251bf0bc6938f2f7a
donsonioc2010 <whddnjs822@gmail.com>
2022-12-03 15:36:26 +0900

    [Spring Boot With Aws][22. 12. 03] - Chapter 04
    - Apply Mustache
    - Make Index And Controller, TestCase

---
+++ src/main/java/com/jongwon/dev/springboot/web/IndexController.java
@@ -0,0 +1,17 @@
+package com.jongwon.dev.springboot.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class IndexController {
+
+  @GetMapping("/")
+  public String index() {
+    /*
+    Mustache Starter 때문에 .mustache는 생략이 된다.
+    앞의 경로는 src/main/resources/templates/가 생략된 상태
+     */
+    return "index";
+  }
+}
